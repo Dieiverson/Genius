a02386160856df62e7612978f8ef59e36bd0f8cf
Dieiverson Kava <Dieiverson@users.noreply.github.com>
2018-10-10 22:44:17 -0300

    Commit Inicial

---
+++ GENIUS/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace GENIUS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.lbl_level = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCoral;
+            this.button1.Location = new System.Drawing.Point(92, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 122);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.button2.Location = new System.Drawing.Point(164, 275);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 58);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.button3.Location = new System.Drawing.Point(318, 150);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 122);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.button4.Location = new System.Drawing.Point(164, 90);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(156, 58);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(2)))));
+            this.button5.FlatAppearance.BorderSize = 12;
+            this.button5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(2)))));
+            this.button5.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(16, 13);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(151, 64);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "INICIAR";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // lbl_level
+            // 
+            this.lbl_level.AutoSize = true;
+            this.lbl_level.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_level.Location = new System.Drawing.Point(311, 25);
+            this.lbl_level.Name = "lbl_level";
+            this.lbl_level.Size = new System.Drawing.Size(110, 37);
+            this.lbl_level.TabIndex = 5;
+            this.lbl_level.Text = "NÍVEL";
+            // 
+            // button6
+            // 
+            this.button6.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(2)))));
+            this.button6.FlatAppearance.BorderSize = 12;
+            this.button6.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(2)))));
+            this.button6.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(181, 175);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(119, 64);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "VERIFICAR";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 402);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.lbl_level);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label lbl_level;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ GENIUS/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GENIUS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbl_level.Text = "Nível: " + QntdCores;
+        }
+
+
+
+
+
+        int[] sequencia = new int[15];
+        int QntdCores = 0;
+        Random aleatorio = new Random();
+        int[] sequenciaEscolhida = new int [15];
+        int acertou = 0;
+
+
+
+
+        
+        private void button5_Click(object sender, EventArgs e)
+        {
+           
+            QntdCores++;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button6.Enabled = true;
+            button6.Enabled = false;
+            button5.Enabled = false;
+            lbl_level.Text = "Nível: " + QntdCores;
+
+
+
+            for (int i = 0; i < QntdCores; i++)
+            {
+                sequencia[i] = aleatorio.Next(1,5);
+            }
+
+            for (int i = 0; i < QntdCores; i++)
+            {
+
+                if (sequencia[i] == 1)
+                {
+                    
+                   
+                    Task.Delay(500).Wait();
+                    button1.BackColor = Color.Red;
+                    Task.Delay(1000).Wait();
+                    button1.BackColor = Color.LightCoral;
+                   
+                    continue;
+                    
+                }
+                if (sequencia[i] == 2)
+                {
+                    
+                   
+                    Task.Delay(500).Wait();
+                    button2.BackColor = Color.Blue;
+                    Task.Delay(1000).Wait();
+                    button2.BackColor = Color.LightSteelBlue;
+                    
+                    continue;
+
+                }
+                if (sequencia[i] == 3)
+                {
+                   
+                    
+                    Task.Delay(500).Wait();
+                    button3.BackColor = Color.Yellow;
+                    Task.Delay(1000).Wait();
+                    button3.BackColor = Color.PaleGoldenrod;
+                   
+                    continue;
+                }
+                if (sequencia[i] == 4)
+                {
+                   
+                   
+                    Task.Delay(500).Wait();
+                    button4.BackColor = Color.Green;
+                    Task.Delay(1000).Wait();
+                    button4.BackColor = Color.DarkSeaGreen;
+                    
+                    continue;
+                }
+
+                 
+
+            }
+            button6.Enabled = true;
+           
+
+        }
+
+
+
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < QntdCores; i++)
+            {
+                if (sequenciaEscolhida[i] == 0)
+                {
+                    sequenciaEscolhida[i] = 4;
+                    break;
+                }
+            }
+            button4.BackColor = Color.Green;
+            Task.Delay(200).Wait();
+            button4.BackColor = Color.DarkSeaGreen;
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < QntdCores; i++)
+            {
+                if (sequenciaEscolhida[i] == 0)
+                {
+                    sequenciaEscolhida[i] = 1;
+                    break;
+                }
+            }
+            button1.BackColor = Color.Red;
+            Task.Delay(200).Wait();
+            button1.BackColor = Color.LightCoral;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < QntdCores; i++)
+            {
+                if (sequenciaEscolhida[i] == 0)
+                {
+                    sequenciaEscolhida[i] = 2;
+                    break;
+                }
+            }
+            button2.BackColor = Color.Blue;
+            Task.Delay(200).Wait();
+            button2.BackColor = Color.LightSteelBlue;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < QntdCores; i++)
+            {
+                if (sequenciaEscolhida[i] == 0)
+                {
+                    sequenciaEscolhida[i] = 3;
+                    break;
+                }
+            }
+            button3.BackColor = Color.Yellow;
+            Task.Delay(200).Wait();
+            button3.BackColor = Color.PaleGoldenrod;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button5.Enabled = true;
+            for (int i = 0; i < QntdCores; i++)
+            {
+                if (sequenciaEscolhida[i] == sequencia[i])
+                {
+                    acertou++;
+                }
+            }
+                if (acertou == QntdCores)
+                {
+
+
+                    MessageBox.Show("Parabéns.Você passou de nível!");
+                    lbl_level.Text = "Nível: " + QntdCores;
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+                    button4.Enabled = false;
+                    button6.Enabled = false;
+                    button5.Text = "Próximo nível";
+                    acertou = 0;
+
+                    for (int j = 0; j < QntdCores; j++)
+                    {
+                        sequencia[j] = 0;
+                        sequenciaEscolhida[j] = 0;
+
+                    }
+                    
+
+
+                }
+
+
+
+
+                else
+                {
+                    MessageBox.Show("Você errou a sequência!");
+                    
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+                    button4.Enabled = false;
+                    button6.Enabled = false;
+                    lbl_level.Text = "Nível: " + QntdCores;
+                    acertou = 0;
+                button5.Text = "INICIAR";
+
+                    for (int k = 0; k < QntdCores; k++)
+                    {
+                        sequencia[k] = 0;
+                        sequenciaEscolhida[k] = 0;
+                    }
+                QntdCores = 0;
+
+            }
+            
+        }
+
+        
+    }
+    }
+
